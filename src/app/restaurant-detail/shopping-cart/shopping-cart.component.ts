20c33c1a2f29401c02efdcc1bf200b452afd18c8
Marcos Vinicius de Moraes Gabriel <mvm.gabriel@gmail.com>
2021-05-28 23:23:35 -0300

    Fim da aula 54 - Carrinho

--- src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
+++ src/app/restaurant-detail/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuItem } from '../menu-item/menu-item.model';
+import { CartItem } from './cart-item.model';
+import { ShoppingCartService } from './shopping-cart.service';
 
 @Component({
   selector: 'mt-shopping-cart',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingCartComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shoppingCartService:ShoppingCartService) { }
 
   ngOnInit() {
   }
 
+  items():any[]{
+      return this.shoppingCartService.items;
+  }
+
+  total():number{
+    return this.shoppingCartService.total()
+  }
+
+  clear(){
+    this.shoppingCartService.clear()
+  }
+
+  removeItem(cItem:CartItem){
+    this.shoppingCartService.removeItem(cItem)
+  }
+
+  addItem(mItem:MenuItem){
+    this.shoppingCartService.addItem(mItem)
+  }
 }
