623b1c0d7d337a194aeae842883552fa90f7c003
Marcos Vinicius de Moraes Gabriel <mvm.gabriel@gmail.com>
2021-05-31 19:37:29 -0300

    Aula 64 - Componente Forma de Pagto

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { RadioOption } from 'app/shared/radio/radio-option.model';
+
 @Component({
   selector: 'mt-order',
   templateUrl: './order.component.html',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderComponent implements OnInit {
 
+  paymentOption:RadioOption[] = [
+    {label: 'Dinheiro', value: 'MON'},
+    {label: 'Cartão de Débito', value: 'DEB'},
+    {label: 'Vale Refeição', value: 'REF'},
+    {label: 'Cartão de Crédito', value: 'CRE'}
+  ]
+
   constructor() { }
 
   ngOnInit() {
   }
-
 }
