4975f18234d6d853473a628bd8868eff653f7766
Marcos Vinicius de Moraes Gabriel <mvm.gabriel@gmail.com>
2021-06-04 19:12:55 -0300

    Fim da refatoração do form (reactive)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shoppin
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
 import { OrderComponent } from './order/order.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { InputComponent } from './shared/input/input.component';
 import { RadioComponent } from './shared/radio/radio.component';
 import { OrderItemsComponent } from './order/order-items/order-items.component';
@@ -53,6 +53,7 @@ import { RatingComponent } from './shared/rating/rating.component';
     BrowserModule,
     HttpModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(ROUTES)
   ],
   providers: [
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
 
@@ -13,6 +14,11 @@ import { OrderService } from './order.service';
 })
 export class OrderComponent implements OnInit {
 
+  patternOnlyNumber:any = /^[0-9]*$/
+  patternEmail:any = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  
+  orderForm: FormGroup
+
   delivery:number = 8
 
   paymentOption:RadioOption[] = [
@@ -22,9 +28,35 @@ export class OrderComponent implements OnInit {
     {label: 'Cartão de Crédito', value: 'CRE'}
   ]
 
-  constructor(private orderService:OrderService, private router:Router) { }
+  constructor(
+    private orderService:OrderService, 
+    private router:Router,
+    private formBuilder:FormBuilder) { }
 
   ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.patternEmail)]),
+      emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.patternEmail)]),
+      address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.patternOnlyNumber)]),
+      optional: this.formBuilder.control(''),
+      paymentOpt: this.formBuilder.control('', [Validators.required]),
+    },{validator: OrderComponent.equalsTo})
+  }
+
+  static equalsTo(group:AbstractControl):{[key:string]:boolean} {
+    const email = group.get('email')
+    const emailConfirmation = group.get('emailConfirmation')
+    if(!email || !emailConfirmation){
+      return undefined
+    }
+
+    if(email.value !== emailConfirmation.value){
+      return {emailNotMatch:true}
+    }
+
+    return undefined
   }
 
   checkOrder(order:Order){
